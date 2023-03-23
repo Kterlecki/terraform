2b278249115ca6d726ff3c05081614e4d44bc05c
Kterlecki <terlecki1995@gmail.com>
2023-03-23 06:09:30 +0000

    add docker install steps

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,24 @@ resource "aws_instance" "example" {
     instance_type           = "t2.micro"
     key_name                = "secretKeyFile"
 
-        
+    security_groups  = [aws_security_group.http_access.name]
+
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install docker -y
+                sudo usermod -aG docker ec2-user
+                sudo service docker start
+                sudo yum install ruby -y
+                sudo yum install wget
+                wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+                chmod +x ./install
+                sudo ./install auto
+                sudo service codedeploy-agent status
+                EOF
+
+    
     user_data_replace_on_change = true
     tags = {
         Name = "env-stage"
