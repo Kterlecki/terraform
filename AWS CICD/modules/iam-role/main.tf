60382b607fc36a9cd09f13027fb3e76a85a77b3f
Kterlecki <terlecki1995@gmail.com>
2023-05-22 07:45:25 +0100

    Add Iam policy and role policy attachment

--- AWS CICD/modules/iam-role/main.tf
+++ AWS CICD/modules/iam-role/main.tf
@@ -41,3 +41,56 @@ resource "aws_iam_role" "codepipeline_role" {
     tag-key = "tag-value"
   }
 }
+
+resource "aws_iam_policy" "codepipeline_policy" {
+  name        = "test_policy_codepipeline"
+  description = "Policy to allow codepipeline to execute"
+  tags = "tag-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObjectAcl",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Effect  = "Allow",
+        Action  = [
+          "s3:GetBucketVersioning"
+        ],
+        Resource = "*"
+      },
+      {
+      Effect  = "Allow",
+      Action  = [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild",
+        "codebuild:BatchGetProjects"
+      ],
+      Resource  = "*"
+      },
+      {
+        Effect  = "Allow",
+        Action  = [
+          "codebuild:CreateReportGroup",
+          "codebuild:CreateReport",
+          "codebuild:UpdateReport",
+          "codebuild:BatchPutTestCases"
+        ],
+        Resource  = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_role_attach" {
+  role  = aws_iam_role.codepipeline_role[0].name
+  policy_arn = aws_iam_policy.codepipeline_policy[0].arn
+}
