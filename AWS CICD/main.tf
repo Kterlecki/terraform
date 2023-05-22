f8157d776d04f9ce080fb16c41d0e78c6abdc5d6
Kterlecki <terlecki1995@gmail.com>
2023-05-22 08:20:47 +0100

    add module reference for iam role codepipeline

--- AWS CICD/main.tf
+++ AWS CICD/main.tf
@@ -3,11 +3,22 @@ terraform {
 }
 
 module "artifact_bucket" {
-    source = "./modules/S3"
-    project_name = "test_project_module"
-    codepipeline_role_arn = ""
+    source                  = "./modules/S3"
+    project_name            = "test_project_module"
+    codepipeline_role_arn   = ""
 
     tags = {
-        Project_Name = "test_project_module"
+        Project_Name        = "test_project_module"
     }
+}
+
+module "codepipeline_iam_role" {
+  source                      = "./modules/iam-role"
+  project_name                = "test_project_module"
+  codepipeline_iam_role_name  = var.codepipeline_iam_role_name
+  s3_bucket_arn               = module.artifact_bucket.arn
+  tags = {
+    Project_Name              = "test_project_module"
+  }
+  
 }
