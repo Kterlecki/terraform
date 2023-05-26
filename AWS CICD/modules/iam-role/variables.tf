d4ce564b803277eed2ac5c2ccef8ab1847a6536d
Kterlecki <terlecki1995@gmail.com>
2023-05-26 07:14:35 +0100

    add more variables to iam role module

--- AWS CICD/modules/iam-role/variables.tf
+++ AWS CICD/modules/iam-role/variables.tf
@@ -2,3 +2,8 @@ variable "codepipeline_iam_role_name" {
     description = "Name of the IAM role to be used for this project"
     type        = string
 }
+
+variable "s3_bucket_arn" {
+  description = "The ARN of S3 bucket"
+  type = string
+}
