8d0360a34a39b7065e8cf59cd7f3bad1230445ea
Kterlecki <terlecki1995@gmail.com>
2023-05-26 08:03:05 +0100

    Add stages to codepipeline

--- AWS CICD/modules/codepipeline/main.tf
+++ AWS CICD/modules/codepipeline/main.tf
@@ -8,4 +8,38 @@ resource "aws_codepipeline" "terraform_pipeline" {
     type = "S3"
 
   }
+
+  stage {
+    name = "Source"
+    action {
+      name = "Download source"
+      category = "Source"
+      owner = "AWS"
+      version = "1"
+      provider = "S3"
+      output_artifacts = [ "source_output" ]
+      run_order = 1
+      configuration = {
+        S3Bucket = "s3-bucket-kt"
+        S3ObjectKey = "hello-world"
+        PollForSourceChanges = "true"
+      }
+    }
+  }
+  stage {
+    name = "Build"
+    action {
+      name = "Build"
+      category = "Build"
+      owner = "AWS"
+      provider = "CodeBuild"
+      input_artifacts = [ "source_output" ]
+      output_artifacts = [ "build_output" ]
+      version = "1"
+      configuration = {
+        ProjectName = "hello-world"
+      }
+    }
+  }
 }
+
