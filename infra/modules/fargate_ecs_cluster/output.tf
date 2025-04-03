36d2b72f6a78506e847532cd899fe3703b49b141
Kacper Terlecki <kacper.terlecki@pfizer.com>
2025-04-03 10:06:09 +0100

    add cluster resources

--- infra/modules/fargate_ecs_cluster/output.tf
+++ infra/modules/fargate_ecs_cluster/output.tf
@@ -1,3 +1,9 @@
 output "cluster_name" {
   value = aws_ecs_cluster.ecs_cluster.name
 }
+output "cluster_id" {
+  value = aws_ecs_cluster.ecs_cluster.id
+}
+output "ecr_task_execution_role_arn" {
+  value = aws_iam_role.ecr_task_execution_role.arn
+}
